a71059529d239b38ea741a27e87c5d9877643612
Tomasz-Gziut <225775888+Tomasz-Gziut@users.noreply.github.com>
2025-10-05 00:55:39 +0200

    /feature added shadcn

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { availableRoutes } from "../App";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
 function Home() {
   const otherRoutes = availableRoutes.filter((route) => route.path !== "/");
@@ -7,12 +8,19 @@ function Home() {
   return (
     <>
       <h1>Cheatsheets:</h1>
+
       <div>
         {otherRoutes.map((route) => (
           <div key={route.path}>
-            <Link className="text-blue-600" to={route.path}>
-              {route.name}
-            </Link>
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <Link className="text-blue-600" to={route.path}>
+                    {route.name}
+                  </Link>
+                </CardTitle>
+              </CardHeader>
+            </Card>
           </div>
         ))}
       </div>
--- src/pages/Route1.tsx
+++ src/pages/Route1.tsx
@@ -1,4 +1,13 @@
 import { Link } from "react-router-dom";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function Route1() {
   return (
@@ -6,21 +15,32 @@ function Route1() {
       <Link to="/" className="text-blue-600">
         Back
       </Link>
-      <div>
-        <p>
-          Zagadnienia na kartkówkę 9.10. Zagadnienia kartkówka Wielka Wojna ·
-          Omów przyczyny wybuchu I wojny światowej *długofalowe (np.
-          imperializm, militaryzm, wyścig zbrojeń, system sojuszy) i
-          bezpośrednie (zamach w Sarajewie) przyczyny wojny. · Omów System
-          sojuszy i plany wojenne przed 1914 r. Trójprzymierze vs
-          Trójporozumienie, składy sojuszy i przyczyny ich zawarcia · Wskaż
-          Najważniejsze wydarzenia i bitwy 1914–1916 bitwa nad Marną (1914),
-          bitwa pod Tannenbergiem, bitwa pod Verdun, bitwa nad Sommą, wojna
-          pozycyjna na Zachodzie, ruchy frontu Wschodniego. · Omów przyczyny i
-          skutki rewolucji lutowej i październikowej. · Wskaż przyczyny
-          przyłączenia Stanów Zjednoczonych do ententy.
-        </p>
-      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Zagadnienia na kartkówkę 9.10.</CardTitle>
+          <CardDescription>Zagadnienia kartkówka Wielka Wojna</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>
+            Omów przyczyny wybuchu I wojny światowej *długofalowe (np.
+            imperializm, militaryzm, wyścig zbrojeń, system sojuszy) i
+            bezpośrednie (zamach w Sarajewie) przyczyny wojny.
+          </p>
+          <p>
+            Omów System sojuszy i plany wojenne przed 1914 r. Trójprzymierze vs
+            Trójporozumienie, składy sojuszy i przyczyny ich zawarcia
+          </p>
+          <p>
+            Wskaż Najważniejsze wydarzenia i bitwy 1914–1916 bitwa nad Marną
+            (1914), bitwa pod Tannenbergiem, bitwa pod Verdun, bitwa nad Sommą,
+            wojna pozycyjna na Zachodzie, ruchy frontu Wschodniego.
+          </p>
+          <p>
+            Omów przyczyny i skutki rewolucji lutowej i październikowej. · Wskaż
+            przyczyny przyłączenia Stanów Zjednoczonych do ententy.
+          </p>
+        </CardContent>
+      </Card>
     </>
   );
 }
